4bd9f82216682edc0578036903f03446e03e19fa
Garry Fishy <agassi.garry@gmail.com>
2022-04-09 20:29:23 +0700

    heroku

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const cors = require('cors');
 const {Customers} = require('./models');
 
-
+require('dotenv').config()
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors())
